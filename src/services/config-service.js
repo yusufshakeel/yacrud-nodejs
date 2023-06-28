80d90d215a752d81daaedde402e203cb876e29a4
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2023-06-28 22:40:58 +0530

    package.json, config, constants

---
+++ src/services/config-service.js
@@ -0,0 +1,21 @@
+'use strict';
+const config = require('config');
+const { ENABLED } = require('../contants');
+module.exports = function () {
+  const environment = config.get('environment');
+  const nodeEnvironment = config.get('nodeEnvironment');
+  const fastify = {
+    keepAliveTimeout: config.get('fastify.keepAliveTimeout'),
+    host: config.get('fastify.host'),
+    port: config.get('fastify.port'),
+    logger: config.get('fastify.loggerEnabled') === ENABLED
+  };
+  const database = {
+    host: config.get('database.host'),
+    port: config.get('database.port'),
+    database: config.get('database.database'),
+    user: config.get('database.user'),
+    password: config.get('database.password')
+  };
+  return { environment, nodeEnvironment, fastify, database };
+};
